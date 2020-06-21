24be6d6ae655f8fa462efb8ae66e09667363f67c
Your Name <cieloeste0303@gmail.com>
2020-06-21 20:33:01 +0900

    update

--- pages/about-me.js
+++ pages/about-me.js
@@ -10,15 +10,36 @@ export default function AboutMe() {
         <h1>Hobbies</h1>
         <h3>Games</h3>
         <h4>Minecraft</h4>
-        <img src="https://lh3.googleusercontent.com/yAtZnNL-9Eb5VYSsCaOC7KAsOVIJcY8mpKa0MoF-0HCL6b0OrFcBizURHywpuip-D6Y" />
+        <div>
+          <img
+            src="https://lh3.googleusercontent.com/yAtZnNL-9Eb5VYSsCaOC7KAsOVIJcY8mpKa0MoF-0HCL6b0OrFcBizURHywpuip-D6Y"
+            style={{ width: '40rem' }}
+          />
+        </div>
+        <a href="https://www.minecraft.net/" target="blank">
+          Official Website
+        </a>
       </div>
-      <p style={{ color: '#fff', padding: '3rem' }}>
+      <p style={{ color: '#fff', padding: '5rem' }}>
         {' '}
         Minecraft is a game that is based on blocks. A block world is generated
         in the game, and you can either build massive structures in creative
         mode or fight monsters, hunt, and survive in survival mode. {''}
       </p>
       <h4>Civilization VI</h4>
+      <img
+        src="https://images8.alphacoders.com/707/thumb-1920-707947.jpg"
+        style={{ width: '50rem' }}
+      />
+      <div>
+        <a href="https://civilization.com/ko-KR/" target="blank">
+          Official Website
+        </a>
+      </div>
+      <p style={{ color: '#fff', padding: '5rem' }}>
+        Civilization VI is a game where you can build your own civilizations,
+        and make your country into the greatest country on Earth.
+      </p>
     </div>
   );
 }
